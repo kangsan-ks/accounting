61e0b19dc11f45e8e49b44173e77e7f90072377c
kimmijin <12rlaalwls@gmail.com>
2021-01-26 16:18:16 +0900

    testtest

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+//ㄴㄴㄴㄴㄴ
 Route::get('/', 'Front@main');
 
 Route::get('/info/greeting', 'Front@info');
